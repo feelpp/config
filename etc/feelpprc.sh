70f200db2439b5986073043ddbc3b6f5bc10f6ff
Guillaume DOLLE <dolle.guillaume@gmail.com>
2014-03-04 12:10:25 +0100

    fix path and default config

--- etc/feelpprc.sh
+++ etc/feelpprc.sh
@@ -10,12 +10,22 @@
 #
 ################################################################################
 
+
+module purge
+
 # Default feel++ local install
 export FEELPP_SHARE_PATH=/usr/local/feelpp/
 
 # Default feel++ module path.
-export FEELPP_MODULE_PATH=/usr/local/feelpp/modules
+export FEELPP_MODULE_PATH=/home/math/gdolle/devel/config/modules/
 
-# Dynamic module path
+# Default dynamic module path
 export MODULEPATH=$MODULEPATH:$FEELPP_MODULE_PATH/files:$FEELPP_MODULE_PATH/profiles
+
+# Custom rules per host
+case "$HOSTNAME" in
+    'hpc-login')
+        module load batch/slurm ;;
+esac
+
 
