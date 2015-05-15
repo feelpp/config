4fb39a3038eeffdcd6c19f9d71900b0d4e050d38
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2015-05-15 16:26:51 +0200

    Bugfix in configure script

--- configure.py
+++ configure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python
 
 import curses
 from curses import panel
@@ -22,6 +22,49 @@ class MenuItem(object):
 
     def getFunction(self):
         return self.function
+
+class DialogYesNo(object):
+    def ask(self, question, screen):
+        window = screen.subwin(0,0)
+        window.keypad(1)
+        pnl = panel.new_panel(window)
+        pnl.hide()
+        panel.update_panels()
+
+        position = 0
+        pnl.top()
+        pnl.show()
+        window.clear()
+
+        bVal = False
+        while True:
+            window.clear()
+            #window.refresh()
+            curses.doupdate()
+
+            lpos = 1
+            window.addstr(lpos, 1, question, curses.A_NORMAL)
+            lpos = lpos + 1
+            if(bVal):
+                window.addstr(lpos, 1, "YES", curses.A_REVERSE)
+                window.addstr(lpos, 10, "NO", curses.A_NORMAL)
+            else:
+                window.addstr(lpos, 1, "YES", curses.A_NORMAL)
+                window.addstr(lpos, 10, "NO", curses.A_REVERSE)
+
+            key = window.getch()
+
+            if key in [curses.KEY_LEFT, curses.KEY_UP]:
+                bVal = True
+            elif key in [curses.KEY_RIGHT, curses.KEY_DOWN]:
+                bVal = False
+            elif key in [curses.KEY_ENTER, ord('\n')]:
+                window.clear()
+                pnl.hide()
+                panel.update_panels()
+                curses.doupdate()
+                return bVal;
+
 
 class MenuItemUpdateDictEntry(object):
     def __init__(self, desc, dct, key, stdscreen):
@@ -96,6 +139,15 @@ class MenuItemModuleList(object):
         self.moduleList = []
         self.activeModuleList = []
 
+        self.buildModuleList(os.path.join(self.dct[self.key], "files", "src"), 0)
+        self.buildActiveModuleList(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"]), 0)
+
+        # Remove base working directory form module name
+        for i in range(len(self.moduleList)):
+            self.moduleList[i] = self.moduleList[i].replace(os.path.join(self.dct[self.key], "files", "src") + "/", "")
+        for i in range(len(self.activeModuleList)):
+            self.activeModuleList[i] = self.activeModuleList[i].replace(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"]) + "/", "")
+
     def getDescription(self):
         return self.desc + ": " + self.dct[self.key]
 
@@ -157,14 +209,14 @@ class MenuItemModuleList(object):
         #    self.position = len(self.moduleList)-1
 
     def update(self):
-        self.buildModuleList(os.path.join(self.dct[self.key], "src"), 0)
-        self.buildActiveModuleList(os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"]), 0)
+        self.buildModuleList(os.path.join(self.dct[self.key], "files", "src"), 0)
+        self.buildActiveModuleList(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"]), 0)
 
         # Remove base working directory form module name
         for i in range(len(self.moduleList)):
-            self.moduleList[i] = self.moduleList[i].replace(os.path.join(self.dct[self.key], "src") + "/", "")
+            self.moduleList[i] = self.moduleList[i].replace(os.path.join(self.dct[self.key], "files", "src") + "/", "")
         for i in range(len(self.activeModuleList)):
-            self.activeModuleList[i] = self.activeModuleList[i].replace(os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"]) + "/", "")
+            self.activeModuleList[i] = self.activeModuleList[i].replace(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"]) + "/", "")
 
         self.panel.top()
         self.panel.show()
@@ -176,7 +228,7 @@ class MenuItemModuleList(object):
             curses.doupdate()
 
             lpos = 1
-            self.window.addstr(lpos, 1, "Module list", curses.A_NORMAL)
+            self.window.addstr(lpos, 1, "Module list (Enter to select, Esc to validate changes and quit this menu)", curses.A_NORMAL)
             lpos = lpos + 1
 
             for index in range(min(len(self.moduleList), (curses.LINES - 5) + 1)):#len(self.moduleList)):
@@ -188,30 +240,30 @@ class MenuItemModuleList(object):
                 if(self.moduleList[index + self.windowStart] in self.activeModuleList):
                     msg = '\t%d. [*] %s' % (index + self.windowStart, self.moduleList[index + self.windowStart])
                 else:
-                    msg = '\t%d. [ ] %s %d %d' % (index + self.windowStart, self.moduleList[index + self.windowStart], len(self.activeModuleList), len(self.moduleList))
+                    msg = '\t%d. [ ] %s' % (index + self.windowStart, self.moduleList[index + self.windowStart])
                 self.window.addstr(lpos, 1, msg, mode)
                 lpos = lpos + 1
 
             key = self.window.getch()
 
             if key in [curses.KEY_ENTER, ord('\n')]:
-                if self.position == len(self.moduleList)-1:
-                    break
+                #print(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart]))
+                if(self.moduleList[self.position + self.windowStart] in self.activeModuleList):
+                    os.remove(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart]))
+                    self.activeModuleList.remove(self.moduleList[self.position + self.windowStart])
                 else:
-                    os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart])
-                    if(self.moduleList[self.position + self.windowStart] in self.activeModuleList):
-                        os.remove(os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart]))
-                        self.activeModuleList.remove(self.moduleList[self.position + self.windowStart])
-                    else:
-                        modpath=os.path.dirname(os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart]))
-                        if(not os.path.exists(modpath)):
-                            os.makedirs(modpath)
-                        os.symlink(
-                        os.path.join(self.dct[self.key], "src", self.moduleList[self.position + self.windowStart]),
-                        os.path.join(self.dct[self.key], Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart])
-                        )
-                        self.activeModuleList.append(self.moduleList[self.position + self.windowStart])
+                    modpath=os.path.dirname(os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart]))
+                    if(not os.path.exists(modpath)):
+                        os.makedirs(modpath)
+                    os.symlink(
+                    os.path.join(self.dct[self.key], "files", "src", self.moduleList[self.position + self.windowStart]),
+                    os.path.join(self.dct[self.key], "files", Settings["FEELPP_HPCNAME"], self.moduleList[self.position + self.windowStart])
+                    )
+                    self.activeModuleList.append(self.moduleList[self.position + self.windowStart])
 
+            # When the user press escape, leave the loop
+            elif key == 27:
+                break
             elif key == curses.KEY_UP:
                 self.navigate(-1)
 
@@ -286,11 +338,44 @@ class Menu(object):
         panel.update_panels()
         curses.doupdate()
 
+def writeActiveModuleList(activeModuleList):
+
+    log = open("log", "w")
+
+    modfile=os.path.join(Settings["FEELPP_CONFIG_PATH"], "etc", "feelpprc.d", Settings["FEELPP_HPCNAME"] + ".sh")
+
+    # open and close the file
+    # in case it doesn't exist
+    f = open(modfile, "a")
+    f.close()
+
+    f = open(modfile, "r")
+    lines = f.readlines()
+    log.write(str(lines))
+    log.write("\n\n")
+
+    content = []
+    for l in lines:
+        if(not l.lstrip().startswith("#") and l.lstrip().translate(None, "\n") != ""):
+            content.append(l.lstrip().translate(None, "\n"))
+    log.write(str(content))
+    log.write("\n\n")
+
+    log.write(str(activeModuleList))
+    log.write("\n\n")
+
+    for m in activeModuleList:
+        root, ext = os.path.splitext(os.path.basename(m))
+        log.write("FEELPP_" + root.upper().translate(None, ".-") + "_PATH=" + Settings["FEELPP_SHARE_PATH"] + "\n")
+
 class MyApp(object):
 
     def __init__(self, stdscreen):
         self.screen = stdscreen
         curses.curs_set(0)
+
+        #d = DialogYesNo()
+        #d.ask("Test", stdscreen)
 
         hpcMenuItems = [
         MenuItemUpdateDictEntry("Cluster name", Settings, "FEELPP_HPCNAME", self.screen),
@@ -305,13 +390,19 @@ class MyApp(object):
         mainMenu = Menu("Module configuration", mainMenuItems, self.screen)
         mainMenu.display()
 
+        writeActiveModuleList(mainMenuItems[1].activeModuleList)
+
 if __name__ == '__main__':
     Settings["FEELPP_HPCNAME"] = socket.gethostname()
     Settings["FEELPP_CONFIG_PATH"] = os.path.dirname(os.path.realpath(__file__))
-    Settings["FEELPP_MODULE_PATH"] = Settings["FEELPP_CONFIG_PATH"] + "/modules/files"
+    Settings["FEELPP_MODULE_PATH"] = Settings["FEELPP_CONFIG_PATH"] + "/modules"
     Settings["FEELPP_SHARE_PATH"] = "/usr/local/feelpp"
 
-    for k, v in iter(Settings.items()):
-        print(k, v)
-
     curses.wrapper(MyApp)
+
+    f = open(os.path.join(Settings["FEELPP_CONFIG_PATH"], "etc", "environment"), "w")
+
+    for k, v in iter(Settings.items()):
+        f.write(k + "=" + v + "\n")
+    f.close()
+
