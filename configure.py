152ce699a96e62c6b319904eb8fcbd74317e5bb8
Guillaume DOLLÉ <dolle.guillaume@gmail.com>
2015-05-22 18:01:07 +0200

    Fix previous commit

--- configure.py
+++ configure.py
@@ -105,7 +105,7 @@ class MenuItemUpdateDictEntry(object):
                 buf = buf[:len(buf)-1]
             elif key in [curses.KEY_ENTER, ord('\n')]:
                 break;
-            elif curses.ascii.isalnum(key) or curses.ascii.ascii('/'):
+            elif curses.ascii.isgraph(key):
                 buf = buf + chr(key)
 
         window.clear()
